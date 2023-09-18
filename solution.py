617c86cc41e68fe40a45ca1f3f216f0fbd5366d1
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-18 11:28:19 -0400

    added comments

--- solution.py
+++ solution.py
@@ -1,4 +1,10 @@
+# ----PART 1: 
+
 with open("abespeech.txt") as file_ref:
     for line_no, line in enumerate(file_ref, 1):
         print(f'{line_no}: {line[::-1]}')
+        
+
+# ----PART 2: 
+
 
