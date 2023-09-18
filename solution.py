54a038b00f826623066e4058f07e5ac8a352dc95
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-18 12:08:42 -0400

    starting logic for part 2

--- solution.py
+++ solution.py
@@ -1,10 +1,16 @@
 # ----PART 1: 
 
-with open("abespeech.txt") as file_ref:
-    for line_no, line in enumerate(file_ref, 1):
-        print(f'{line_no}: {line[::-1]}')
+# with open("abespeech.txt") as file_ref:
+#     for line_no, line in enumerate(file_ref, 1):
+#         print(f'{line_no}: {line[::-1]}')
         
 
 # ----PART 2: 
 
-
+with open("smedlybutler.txt", encoding="utf8") as fn:
+    lst = []
+    for line in fn:
+        for word in line.split():
+            if len(word) > 9:
+                lst.append(word)
+                print(lst)
